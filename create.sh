828840d65c9c3ccdde243ee3a152231c8fd8e1bb
Klaus Wenger <kjwenger@yahoo.com>
2021-07-08 06:14:09 +0200

    initial load

---
+++ create.sh
@@ -0,0 +1,6 @@
+mvn archetype:generate \
+	-DgroupId=com.u14 \
+	-DartifactId=sandbox.java.modbus \
+	-DarchetypeArtifactId=maven-archetype-quickstart \
+	-DarchetypeVersion=1.4 \
+	-DinteractiveMode=false
